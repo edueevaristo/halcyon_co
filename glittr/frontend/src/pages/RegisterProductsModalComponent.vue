0967ffd489637e687b51cd7a92f07de5bcb495ba
edueevaristo <eduevaristo_dev@hotmail.com>
2025-04-19 18:23:33 -0300

    [feature/sm-treinamento-git] - Commit Feature | Treinamento git!

---
+++ glittr/frontend/src/pages/RegisterProductsModalComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-modal-product-register-component">
+
+    <article class="content">
+
+    </article>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'RegisterProductsModal',
+
+};
+
+</script>
+
+<style scoped>
+</style>
