a7ae4f0390d19bbb62ae33d4b1f439b1d69a7ed9
dannielnotfound <danielriber548@gmail.com>
2025-05-27 20:34:43 -0300

    Product Review

---
+++ glittr/backend/database/migrations/2025_05_27_225855_create_product_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Product;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->text('comment');
+            $table->unsignedTinyInteger('stars'); // de 1 a 5
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_reviews');
+    }
+};
