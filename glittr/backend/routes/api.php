28eddf5eabd049cb9fb499e091847faf45294d1a
edueevaristo <eduevaristo_dev@hotmail.com>
2025-06-04 03:58:18 -0300

    [feature/sm-ajuste-api] - Commit Feature | Ajuste rotas

--- glittr/backend/routes/api.php
+++ glittr/backend/routes/api.php
@@ -16,12 +16,10 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/products', [ProductsController::class, 'insert']);
-    Route::put('/products', [ProductsController::class, 'update']);
-    Route::put('/reviews/{id}', [ProductReviewController::class, 'update']);
-    Route::delete('/reviews/{id}', [ProductReviewController::class, 'destroy']);
 });
 
+
+/* Rotas de produto sem necessidade de login */
 Route::prefix('products')->group(function () {
 
     Route::get('/', [ProductsController::class, 'index']);
@@ -32,14 +30,19 @@ Route::prefix('products')->group(function () {
 
 Route::post('/products', [ProductsController::class, 'insert']);
 Route::put('/products', [ProductsController::class, 'update']);
-Route::post('/reviews', [ProductReviewController::class, 'store']);
-Route::get('/products/{product}/reviews', [ProductReviewController::class, 'index']);
-Route::get('/reviews/{id}', [ProductReviewController::class, 'show']);
 
 Route::prefix('categories')->group(function () {
 
     Route::get('/', [ProductsController::class, 'index']);
     Route::get('show/{id}', [ProductsController::class, 'show']);
+});
+
+
+Route::prefix('categories')->group(function () {
+
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::get('show/{id}', [CategoryController::class, 'show']);
+
 });
 
 Route::prefix('subcategories')->group(function () {
@@ -55,5 +58,65 @@ Route::prefix('attributes')->group(function () {
     Route::get('show/{id}', [AttributeController::class, 'show']);
     Route::get('values/{id}', [AttributeController::class, 'showValues']);
     Route::get('show/subcategory/{id}', [AttributeController::class, 'showValuesBySubcategory']);
+
 });
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::prefix('products')->group(function () {
+
+    Route::get('/', [ProductsController::class, 'index']);
+    Route::get('show/{id}', [ProductsController::class, 'show']);
+});
+
+Route::post('/products', [ProductsController::class, 'insert']);
+Route::put('/products', [ProductsController::class, 'update']);
+
+/* Rotas de produto sem necessidade de login */
+Route::prefix('categories')->group(function () {
+
+    Route::get('/', [ProductsController::class, 'index']);
+    Route::get('show/{id}', [ProductsController::class, 'show']);
+});
+
+Route::prefix('categories')->group(function () {
+
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::get('show/{id}', [CategoryController::class, 'show']);
+
+});
+
+Route::prefix('subcategories')->group(function () {
+
+    Route::get('/', [SubcategoryController::class, 'index']);
+    Route::get('show/{id}', [SubcategoryController::class, 'show']);
+
+});
+
+Route::prefix('attributes')->group(function () {
+
+    Route::get('/', [AttributeController::class, 'index']);
+    Route::get('show/{id}', [AttributeController::class, 'show']);
+    Route::get('values/{id}', [AttributeController::class, 'showValues']);
+    Route::get('show/subcategory/{id}', [AttributeController::class, 'showValuesBySubcategory']);
+
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::put('/reviews/{id}', [ProductReviewController::class, 'update']);
+    Route::delete('/reviews/{id}', [ProductReviewController::class, 'destroy']);
+});
+
+Route::post('/reviews', [ProductReviewController::class, 'store']);
+Route::get('/products/{product}/reviews', [ProductReviewController::class, 'index']);
+Route::get('/reviews/{id}', [ProductReviewController::class, 'show']);
 
