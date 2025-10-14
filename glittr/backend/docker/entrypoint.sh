84e871e8572328d71771c9190bc7694cbda0fb41
Eduardo Evaristo <91930037+edueevaristo@users.noreply.github.com>
2025-10-13 23:58:19 -0300

    Update entrypoint.sh

--- glittr/backend/docker/entrypoint.sh
+++ glittr/backend/docker/entrypoint.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+# !/bin/sh
 
+# Cria diretórios temporários
 mkdir -p /tmp/nginx/body
 chmod -R 777 /tmp/nginx
 
+# Substitui variável PORT e salva em /tmp
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /tmp/default.conf
-mv /tmp/default.conf /etc/nginx/conf.d/default.conf
 
+# Roda PHP-FPM
 php-fpm -D
-nginx -g 'daemon off;'
+
+# Roda Nginx apontando para o conf temporário
+nginx -g 'daemon off;' -c /tmp/default.conf
