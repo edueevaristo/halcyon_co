102b64adf545dbc4a7c92d92c181d793fea06baa
Eduardo Evaristo <91930037+edueevaristo@users.noreply.github.com>
2025-10-13 23:37:46 -0300

    Update entrypoint.sh

--- glittr/backend/docker/entrypoint.sh
+++ glittr/backend/docker/entrypoint.sh
@@ -3,6 +3,8 @@
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
 mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 
+mkdir -p /var/lib/nginx/body
+chmod -R 777 /var/lib/nginx
 
 php-fpm -D
 nginx -g 'daemon off;'
