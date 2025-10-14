4fbcbfbab3477ff630d6ce7ad1bf58fa24027686
Eduardo Evaristo <91930037+edueevaristo@users.noreply.github.com>
2025-10-13 23:22:52 -0300

    Create entrypoint.sh

---
+++ glittr/backend/docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+
+
+php-fpm -D
+nginx -g 'daemon off;'
